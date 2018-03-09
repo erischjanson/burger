19290ba4b9f2a1639f4b1a1516e714f3736da3e1
Elizabeth <erischjanson@gmail.com>
2018-03-09 14:13:01 -0600

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES("cheeseburger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES("veggieburger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES("ostrichburger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES("bigmac", false);
+
