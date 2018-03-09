5f8186b6391b03863799b8026d9e74585ff0d819
Elizabeth <erischjanson@gmail.com>
2018-03-09 15:01:52 -0600

    added query functions to orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,52 @@
 var connection = require("./connection.js");
+
+
+var orm = {
+
+	selectAll: function(tableInput, callback){
+		var queryString = "SELECT * FROM " + tableInput + ";";
+		connection.query(queryString, function(error, results){
+			if(error){
+				throw err;
+			}
+			callback(results);
+		});
+	},
+	insertOne: function(tableInput, columns, values, callback){
+		var queryString = "INSERT INTO " + tableInput;
+		queryString += "(";
+		queryString += columns.toString();
+		queryString += ")";
+		queryString += "VALUES(?)";
+
+		console.log(queryString);
+
+		connection.query(queryString, values, function(error, results){
+			if(error){
+				throw error;
+			}
+			cb(results);
+		});
+
+	},
+	updateOne: function(tableInput, objectColumnValues, condition, callback){
+		var queryString = "UPDATE " + tableInput;
+		queryString += "SET ";
+		queryString += "devoured = true";
+		queryString += "WHERE";
+		queryString += condition;
+
+		console.log(queryString);
+
+		connection.query(queryString, function(error, results){
+			if(error){
+				throw error;
+			}
+			cb(results);
+		});
+	}
+};
+
+module.exports = orm;
+
+
